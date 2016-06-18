2cd779741633ee56aaa23f1ba62debbb2426ed52
Gowtham Kuntumalla <gowthamkuntumalla@gmail.com>
2016-06-18 21:58:49 +0530

    Add files via upload

--- 2d_algo_fft.hpp
+++ 2d_algo_fft.hpp
@@ -1,5 +1,98 @@
 #ifndef _2D_ALGO_FFT_HPP_
 #define _2D_ALGO_FFT_HPP_
+#include <cmath>
+#include <bitset>
+using namespace std;
+using namespace cv;
+
+/*********************** Auxiliary Functions ***********************/
+double avg(const int x2,const int y2, Mat image,const int x1,const int y1)//average of sub matrix
+{
+    double sum=0.0;
+    for (int i=x1; i<x2; i++) //window starting at (x,y)
+    {
+        for (int j=y1; j<y2; j++) // <i><x><col> equivalent and similar
+        {
+            double a = (int)image.at<uchar> (j,i);//(row,column)
+            sum+=a;
+        }
+    }
+    sum/=((x2-x1)*(y2-y1));
+    return sum;
+}
+double sd(const int x2,const int y2, Mat image,const int x1,const int y1)//standard deviation of sub matrix
+{
+    double var= 0.0;//variance
+    double aver = avg(x2,y2,image,x1,y1);
+    //myfile<<aver<<endl;
+    for (int i=x1; i<x2; i++) //window starting at (x,y)
+    {
+        for (int j=y1; j<y2; j++)
+        {
+            double a = (int)image.at<uchar> (j,i);//(row,column)
+            var+=((a-aver)*(a-aver));
+        }
+    }
+    var=sqrt(var)/((x2-x1)*(y2-y1));
+    return var;
+}
+void max_coef(vector< vector<double> > t,const int wincol,const int winrow,int& max_x,int& max_y )
+{
+    int a=-1;// all intensities are greater than this
+    int m=max_x,n=max_y;
+    for(int i=m; i<wincol+m; i++)
+    {
+        for(int j=n; j<winrow+n; j++)
+        {
+            if(t[j][i]>a)//(row,column)
+            {
+                a=t[j][i];//(row,column)
+                max_x=i;
+                max_y=j;
+            }
+        }
+    }
+    return;
+}
+/** Fast Fourier Transform **/
+void fft(Mat img,vector< vector <pair<double,double> > > &ft_img,int c,int r)//image to fourier transformed 2D vector
+{
+    bitset</** 4 for 16, 5 for 32 etc**/> b1{/**number**/};
+}
+void inv_fft(vector< vector <pair<double,double> > > &ft_img,vector< vector <int> > &inv_ft_img)//fourier transformed 2D vector to image(2D vector)
+{
+
+}
+void piv_2d_fft(Mat image1, Mat image2,vector< vector <pair<int,int> > > &max_points,int win_size)
+{
+    /******This is valid for displacements of maximum A/3 where A is window_Size  ******/
+    ofstream myfile; // like 'cout' it outputs to a file
+    int initial_value = 0;
+    double avg1=0,avg2=0;
+    double sd1=0, sd2=0;
+    int totrow1= image1.rows,totcol1=image1.cols; //opencv functions to get the rows and columns of image.
+    int totrow2= image2.rows,totcol2=image2.cols;
+
+    vector< vector <pair<double,double> > > /**/; //Fourier transform For image1
+    vector< vector <pair<double,double> > > /**/; //For image2
+    vector< vector <pair<double,double> > > /**/; //For product (I1)*(I2')
+    /**/ = vector<vector<double> >(totrow1, vector<double>(totcol1,initial_value));// initialization
+    /** Loop over entire image**/
+    for(int c=0; c</**/; c+=/**/) // according to win_size
+    {
+        for(int r=0; r</**/; r+=/**/)
+        {
+            /**do FFT**/
+            //bit reversal int b=a>>1;
+            /**calculate i1*(i2')**/
+            /**do inverse FFT**/
+            /**Save the data in a "data_i.txt" file**/
+        }
+    }
+    return;
+}
+
+/******* Declarations *******/
 
 #endif // _2D_ALGO_FFT_HPP_
 
--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 #include <opencv2/imgproc/imageproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <string>
 #include "2d_algo_fft.hpp"
-using namespace std;
+
 /** This is "2D Particle image velocimetry, Version " **/
 
 /** mhkhaniitb@gmail.com {PhD}, Guide **/
@@ -11,6 +12,40 @@ using namespace std;
 /** Output data file consists of initial point(x1,y1) final point(x2,y2) **/
 int main()
 {
-cout<<"Hello world"<<'\n';
-return 0;
+    // Import images
+    int win_size;
+    cout<<"Enter the window size A x A (give only A which is a power of 2)"<<endl;//give only 16 or 32 or 64
+    cin>>win_size;// window size 'A'
+    for(/*all images*/)
+    {
+        /**************Declarations**************/
+        Mat image1 = imread("image1.tif",0);
+        Mat image2 = imread("image2.tif",0);
+        Mat img(Mat(nHeight, nWidth, CV_8U));//blank black image
+        img=Scalar(0);//background color
+        vector< vector <pair<int,int> > > max_coef_point;
+        int totrows1= image1.rows,totcols1=image1.cols; //opencv functions to get the rows and columns of image.
+        int totrows2= image2.rows,totcols2=image2.cols;
+
+        /**computations**/
+
+        piv_2d_fft(image1,image2,max_coef_point,win_size);// main function
+
+        /**Plot the displacements in a new image**/
+        cv::namedWindow( "Display window", CV_WINDOW_AUTOSIZE );
+        for(int c=0; c</**/; c+=/**/)
+        {
+            for(int r=0; r</**/; r+=/**/)
+            {
+                Point pt1 = Point(c+/**/,r+/**/);//here (x,y)=(col,row) point in cartesian coordiante system
+                Point pt2 = Point(max_coef_point[r][c].second+/**/,max_coef_point[r][c].first+/**/);
+                //syntax =  void arrowedLine(Mat& img, Point pt1, Point pt2, const Scalar& color, int thickness=1, int line_type=8, int shift=0, double tipLength=0.1)
+                arrowedLine(img, pt1, pt2, Scalar(255), 1, 8, 0, .2);//white
+            }
+        }
+        //cv::imshow( "Display window",img);
+        imwrite("result_img"+to_string(/**i**/)+".tif",img);
+        //waitKey(0);// wait for a keystroke in the window
+    }
+    return 0;
 }
